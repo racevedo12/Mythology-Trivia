75769053c02148eb1c34864f093e0320ae208633
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-08-20 14:25:40 -0400

    Added a div with different tags and a button for the questions review

--- Project1/app.js
+++ Project1/app.js
@@ -2,11 +2,37 @@
 
 const inputButton = document.querySelector("#input-button");
 const containerDiv = document.querySelector(".container");
+const questions = [];
 
 const previewQuestions = () =>
 {
-    console.log("Hello There")
+    const previewDiv = document.createElement("div");
+    previewDiv.setAttribute("id", "preview-div");
+
+    const questionTag = document.createElement("h3");
+    questionTag.innerText = "Question Test";
+
+    const answerTag = document.createElement("h3");
+    answerTag.innerText = "Answer Test";
+
+    const previewButton = document.createElement("button");
+    previewButton.setAttribute("id", "preview-button");
+    previewButton.innerText = "Ready to Take Trivia";
+
+    previewButton.addEventListener("click", startGame);
+
+    previewDiv.append(questionTag);
+    previewDiv.append(answerTag);
+    previewDiv.append(previewButton);
+
+    document.body.classList.toggle("popup-color");
+    containerDiv.append(previewDiv);
+
 };
 
+// const startGame = () =>
+// {
+
+// };
 
 inputButton.addEventListener("click", previewQuestions);
