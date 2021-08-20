32d22fd5a174aaf43a28eeaaeb952378fc6521e5
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-08-20 13:33:38 -0400

    Added a new div as a container in the main tag and created the first 2 variables for the input button and the container div

--- Project1/app.js
+++ Project1/app.js
@@ -1,1 +1,12 @@
-console.log("Hello There");
+// console.log("Hello There");
+
+const inputButton = document.querySelector("#input-button");
+const containerDiv = document.querySelector(".container");
+
+// const previewQuestions = () =>
+// {
+
+// };
+
+
+// inputButton.addEventListener("click", previewQuestions);
